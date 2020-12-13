8ac86f63c6fedd8c342bd2fcc5b1efaaa753acb0
cocoball02 <seokho9522@gmail.com>
2020-12-13 21:10:32 +0900

    NumberBaseball in hooks Component done

--- zerocho/chap03/BaseballGame/client.jsx
+++ zerocho/chap03/BaseballGame/client.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NumberBaseball from './NumberBaseball';
-
-ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
+import HooksBaseball from './hooks/NumberBaseball';
+ReactDOM.render(
+  <>
+    <NumberBaseball /> <HooksBaseball />
+  </>,
+  document.querySelector('#root'),
+);
